07e375b86b859301caf3e6631a9e6966c89e3e98
jlowy95 <jlowy95@gmail.com>
2020-01-18 09:59:52 -0800

    Variables and file opening

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,9 +8,31 @@
 # 4. greatest increase in profits (date and amount) over the entire period
 # 5. greatest decrease in losses (date and amount) over the entire period
 
+
 #OS Library for compatibility across platforms reading file paths
 import os
 #CSV Library for use of the .csv data files
 import csv
 
+
+#Set the file path to "budget_data.csv" as "bd_filepath"
+bd_filepath as os.path.join("budget_data.csv")
+
+#Variables
+#Initialize month counter to 0
+months_count = 0
+#Initialize profit/loss total to 0
+net_total = 0
+#Average will be calculated as: (end value - initial value)/months_count
+#These values will be assigned within the csv file work loop
+#Change in p/l will be recorded by "delta_pl"
+#Greatest increase/decrease in p/l will be recorded by "great_p" and 
+#"great_l" respectively.  Their accompanying months will be recorded by
+#"great_p_month" and "great_l_month"
+
+
+#Open the data file to be read as "budget_data"
+with open(bd_filepath, newline="") as budget_data:
+
+    csvreader = csv.reader(budget_data, delimiter=",")
 
