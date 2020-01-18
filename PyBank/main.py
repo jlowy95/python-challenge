9ab3d58cc80f55a0caca6f3d7e0ed410f7225bba
jlowy95 <jlowy95@gmail.com>
2020-01-18 09:45:52 -0800

    Copy instructions

---
+++ PyBank/main.py
@@ -0,0 +1,16 @@
+#PyBank Challenge
+#
+#Create a Python script that analyzes the records
+# to calculate each of the following:
+# 1. total number of months included in the dataset
+# 2. net total amount of "Profit/Losses" over the entire period
+# 3. average of the changes in "Profit/Losses" over the entire period
+# 4. greatest increase in profits (date and amount) over the entire period
+# 5. greatest decrease in losses (date and amount) over the entire period
+
+#OS Library for compatibility across platforms reading file paths
+import os
+#CSV Library for use of the .csv data files
+import csv
+
+
